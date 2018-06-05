4a320ee3e0204db9f1c344980a9a16706c3f17b3
cristhofermf <cristhofermontalvo@gmail.com>
2018-06-05 08:36:27 -0500

    Fin de clase Conexion

---
+++ Capas/SlnCapas/CapaDato/ConexionCD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Agregar NameSpace
+using System.Data.SqlClient;
+
+namespace CapaDato
+{
+    public class ConexionCD
+    {
+        public SqlConnection ConectarSQL()
+        {
+            //Intanciar SqlConnectionStringBuilder
+            SqlConnectionStringBuilder cadenaConexion = new SqlConnectionStringBuilder();
+            cadenaConexion.DataSource = @"DESKTOP-QDCMGIV\MS_SQL";
+            cadenaConexion.InitialCatalog = "BD_2018FE401";
+            cadenaConexion.UserID = "sa";
+            cadenaConexion.Password = "181003";
+
+            //crear objeto de conexion
+            SqlConnection connBD = new SqlConnection(cadenaConexion.ConnectionString);
+            return connBD;
+
+        }
+    }
+}
